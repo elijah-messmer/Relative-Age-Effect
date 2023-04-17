532a4b6ba3a1334c731019457efee261b75de609
elijah-messmer <122414198+elijah-messmer@users.noreply.github.com>
2023-04-16 21:28:29 -0400

    Final Project Update

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const slide2 = d3.select("#g-ai1-3");
 const slide3 = d3.select("#g-ai1-1");
 const slide4 = d3.select("#g-ai1-4");
 const introSVG = d3.select("#g-v2-intro-text-Desktop-intro-Layer-img"); 
-const headerImg = d3.select("#g-v2-intro-text-Desktop-intro-img");
+const headerImg = d3.select("#intro-desktop");
 
 // Scrollytelling Intro Mobile //
 const titleMobile = d3.select("#g-ai1-m5");
@@ -16,7 +16,7 @@ const slide2Mobile = d3.select("#g-ai1-m2");
 const slide3Mobile = d3.select("#g-ai1-m4");
 const slide4Mobile = d3.select("#g-ai1-m3");
 const introSVGMobile = d3.select("#g-mobile-scroller-Mobile-scroller-scroller-mobile-img");
-const headerImgMobile = d3.select("#g-mobile-scroller-Mobile-scroller-img");
+const headerImgMobile = d3.select("#intro-mobile");
 
 //Display landing image
 // Desktop
@@ -125,20 +125,24 @@ d3.select("#step-three").on('stepin', function() {
   });
 // // End Scrollytelling Intro //
 
+
 // Start of Question Section //
+// Hide question swithces
+d3.selectAll("#q1-2-desktop, #text-2, #path-2, #text-3, #path-3, .check1, .check2, #text-2M, #q1-2-mobile, #text-5, #path-5, #text-6, #path-6, #q2-2-desktop, .check3, .check4, #text-6M, #q2-2-mobile, #q3-2-desktop, #text-8, #path-8, .check5, .check6, #text-8M, #q3-2-mobile, .check7, .check8, #text-10, #path-10, #q4-2-desktop, #text-10M, #q4-2-mobile").style('opacity', 0);
 
+// Set up condition for questions 2-4
+let q1Selection = null;
+    document.getElementById('q1-1').addEventListener('click', function() {
+      q1Selection = 'yes';
+      console.log(q1Selection);
+    });
+    document.getElementById('q1-2').addEventListener('click', function() {
+      q1Selection = 'no';
+      console.log(q1Selection);
+    });
 
-d3.selectAll("#brush-img, #text-2, #path-2, #text-3, #path-3, .check1, .check2, #text-2M, #brush-imgM").style('opacity', 0);
-
-const button = document.querySelector('#q1-1');
-const blueTest = document.querySelector('#blueTest');
-
-button.addEventListener('click', () => {
-  blueTest.style.animationPlayState = 'running';
-});
-
-// add event listeners to buttons
-document.querySelectorAll('section[id^="q1-"]').forEach(section => {
+// add event listener for section buttons
+document.querySelectorAll('section[id^="q"]').forEach(section => {
   section.addEventListener('click', () => {
     const sectionId = section.id;
 
@@ -146,31 +150,93 @@ document.querySelectorAll('section[id^="q1-"]').forEach(section => {
     if (sectionId === 'q1-1') {
       document.getElementById('q1-yes-response').classList.remove('hidden');
       document.getElementById('q1-no-response').classList.add('hidden');
-      d3.selectAll("#text-1, #path-1, .check2, #text-1M").transition().duration(500).style('opacity', 0);
-      d3.selectAll("#text-2, #path-2, #text-3, #path-3, #brush-img, #brush-imgM, .check1, #text-2M").transition().duration(500).style('opacity', 1);
+      d3.selectAll("#text-1, #path-1, .check2, #text-1M").transition().duration(700).style('opacity', 0);
+      d3.selectAll("#text-2, #path-2, #text-3, #path-3, #q1-2-desktop, #q1-2-mobile, .check1, #text-2M").transition().duration(700).style('opacity', 1);
+      // reset sections 2-4
+      d3.selectAll(".check3, .check4, .check5, .check6, .check7, .check8, #text-5, #path-5, #text-6, #path-6, #q2-2-desktop, #q2-2-mobile, #text-6M, #text-8, #path-8, #text-9, #path-9, #q3-2-desktop, #q3-2-mobile, #text-9M, #text-10, #path-10, #q4-2-desktop, #q4-2-mobile, #text-10M").style('opacity', 0);
+      d3.selectAll("#text-4, #path-4, #q2-1-desktop, #q2-1-mobile, #text-4M, #text-7, #path-7, #q3-1-desktop, #q3-1-mobile, #text-7M, #text-9, #path-9, #q4-1-desktop, #q4-1-mobile, #text-9M").style('opacity', 1);
+      d3.selectAll("#q2-yes-response-1, #q2-yes-response-2, #q2-no-response-1, #q2-no-response-2, #q3-yes-response-1, #q3-yes-response-2, #q3-no-response-1, q3-no-response-2, #q4-yes-response-1, #q4-yes-response-2, #q4-no-response-1, q4-no-response-2").attr('class', 'hidden');
     } else if (sectionId === 'q1-2') {
       document.getElementById('q1-no-response').classList.remove('hidden');
       document.getElementById('q1-yes-response').classList.add('hidden');
-      d3.selectAll("#text-1, #path-1, .check1, #text-1M").transition().duration(500).style('opacity', 0);
-      d3.selectAll("#text-2, #path-2, #text-3, #path-3, #brush-img, #brush-imgM, .check2, #text-2M").transition().duration(500).style('opacity', 1);
-    } else if (buttonId === 'q2-yes') {
-      document.getElementById('q2-yes-response').classList.remove('hidden');
-      document.getElementById('q2-no-response').classList.add('hidden');
-    } else if (buttonId === 'q2-no') {
-      document.getElementById('q2-no-response').classList.remove('hidden');
-      document.getElementById('q2-yes-response').classList.add('hidden');
-    } else if (buttonId === 'q3-yes') {
-      document.getElementById('q3-yes-response').classList.remove('hidden');
-      document.getElementById('q3-no-response').classList.add('hidden');
-    } else if (buttonId === 'q3-no') {
-      document.getElementById('q3-no-response').classList.remove('hidden');
-      document.getElementById('q3-yes-response').classList.add('hidden');
-    } else if (buttonId === 'q4-yes') {
-      document.getElementById('q4-yes-response').classList.remove('hidden');
-      document.getElementById('q4-no-response').classList.add('hidden');
-    } else if (buttonId === 'q4-no') {
-      document.getElementById('q4-no-response').classList.remove('hidden');
-      document.getElementById('q4-yes-response').classList.add('hidden');
-    }
+      d3.selectAll("#text-1, #path-1, .check1, #text-1M").transition().duration(700).style('opacity', 0);
+      d3.selectAll("#text-2, #path-2, #text-3, #path-3, #q1-2-desktop, #q1-2-mobile, .check2, #text-2M").transition().duration(700).style('opacity', 1);
+      // reset sections 2-4
+      d3.selectAll(".check3, .check4, .check5, .check6, .check7, .check8, #text-5, #path-5, #text-6, #path-6, #q2-2-desktop, #q2-2-mobile, #text-6M, #text-8, #path-8, #text-9, #path-9, #q3-2-desktop, #q3-2-mobile, #text-9M, #text-10, #path-10, #q4-2-desktop, #q4-2-mobile, #text-10M").style('opacity', 0);
+      d3.selectAll("#text-4, #path-4, #q2-1-desktop, #q2-1-mobile, #text-4M, #text-7, #path-7, #q3-1-desktop, #q3-1-mobile, #text-7M, #text-9, #path-9, #q4-1-desktop, #q4-1-mobile, #text-9M").style('opacity', 1);
+      d3.selectAll("#q2-yes-response-1, #q2-yes-response-2, #q2-no-response-1, #q2-no-response-2, #q3-yes-response-1, #q3-yes-response-2, #q3-no-response-1, q3-no-response-2, #q4-yes-response-1, #q4-yes-response-2, #q4-no-response-1, q4-no-response-2").attr('class', 'hidden');
+    } else if (sectionId === 'q2-1') {
+      if (q1Selection === 'yes') {
+        // show response message 1 for q2 if user answered yes to q1
+        document.getElementById('q2-no-response-1').classList.remove('hidden');
+        d3.selectAll('#q2-yes-response-1, #q2-yes-response-2, #q2-no-response-2').attr('class', 'hidden');
+      } else {
+        // show response message 2 for q2 if user answered no to q1
+        document.getElementById('q2-no-response-2').classList.remove('hidden');
+        d3.selectAll('#q2-yes-response-1, #q2-yes-response-2, #q2-no-response-1').attr('class', 'hidden');
+      }
+      d3.selectAll("#text-4, #path-4, .check4, #q2-1-desktop, #q2-1-mobile, #text-4M").transition().duration(700).style('opacity', 0);
+      d3.selectAll("#text-5, #path-5, #text-6, #path-6, .check3, #q2-2-desktop, #q2-2-mobile, #text-6M").transition().duration(700).style('opacity', 1);
+    } else if (sectionId === 'q2-2') {
+      if (q1Selection === 'yes') {
+        // show response message 1 for q2 if user answered yes to q1
+        document.getElementById('q2-yes-response-1').classList.remove('hidden');
+        d3.selectAll('#q2-yes-response-2, #q2-no-response-1, #q2-no-response-2').attr('class', 'hidden');
+      } else {
+        // show response message 2 for q2 if user answered no to q1
+        document.getElementById('q2-yes-response-2').classList.remove('hidden');
+        d3.selectAll('#q1-yes-response-2, #q2-no-response-1, #q2-no-response-2').attr('class', 'hidden');
+      }
+      d3.selectAll("#text-4, #path-4, .check3, #q2-1-desktop, #q2-1-mobile, #text-4M").transition().duration(700).style('opacity', 0);
+      d3.selectAll("#text-5, #path-5, #text-6, #path-6, .check4, #q2-2-desktop, #q2-2-mobile, #text-6M").transition().duration(700).style('opacity', 1);
+    } else if (sectionId === 'q3-1') {
+      if (q1Selection === 'yes') {
+        // show response message 1 for q3 if user answered yes to q1
+        document.getElementById('q3-no-response-1').classList.remove('hidden');
+        d3.selectAll('#q3-yes-response-1, #q3-yes-response-2, #q3-no-response-2').attr('class', 'hidden');
+      } else {
+        // show response message 2 for q3 if user answered no to q1
+        document.getElementById('q3-no-response-2').classList.remove('hidden');
+        d3.selectAll('#q3-yes-response-1, #q3-yes-response-2, #q3-no-response-1').attr('class', 'hidden');
+      }
+      d3.selectAll("#text-7, #path-7, .check6, #q3-1-desktop, #q3-1-mobile, #text-7M").transition().duration(700).style('opacity', 0);
+      d3.selectAll("#text-8, #path-8, #text-9, #path-9, .check5, #q3-2-desktop, #q3-2-mobile, #text-9M").transition().duration(700).style('opacity', 1);
+    } else if (sectionId === 'q3-2') {
+      if (q1Selection === 'yes') {
+        // show response message 1 for q3 if user answered yes to q1
+        document.getElementById('q3-yes-response-1').classList.remove('hidden');
+        d3.selectAll('#q3-yes-response-2, #q3-no-response-1, #q3-no-response-2').attr('class', 'hidden');
+      } else {
+        // show response message 2 for q3 if user answered no to q1
+        document.getElementById('q3-yes-response-2').classList.remove('hidden');
+        d3.selectAll('#q1-yes-response-2, #q3-no-response-1, #q3-no-response-2').attr('class', 'hidden');
+      }
+      d3.selectAll("#text-7, #path-7, .check5, #q3-1-desktop, #q3-1-mobile, #text-7M").transition().duration(700).style('opacity', 0);
+      d3.selectAll("#text-8, #path-8, #text-9, #path-9, .check6, #q3-2-desktop, #q3-2-mobile, #text-9M").transition().duration(700).style('opacity', 1);
+    } else if (sectionId === 'q4-1') {
+      if (q1Selection === 'yes') {
+      // show response message 1 for q4 if user answered yes to q1
+      document.getElementById('q4-no-response-1').classList.remove('hidden');
+      d3.selectAll('#q4-yes-response-1, #q4-yes-response-2, #q4-no-response-2').attr('class', 'hidden');
+      } else {
+      // show response message 2 for q4 if user answered no to q1
+      document.getElementById('q4-no-response-2').classList.remove('hidden');
+      d3.selectAll('#q4-yes-response-1, #q4-yes-response-2, #q4-no-response-1').attr('class', 'hidden');
+      }
+      d3.selectAll("#text-9, #path-9, .check8, #q4-1-desktop, #q4-1-mobile, #text-9M").transition().duration(700).style('opacity', 0);
+      d3.selectAll("#text-10, #path-10, .check7, #q4-2-desktop, #q4-2-mobile, #text-10M").transition().duration(700).style('opacity', 1);
+      } else if (sectionId === 'q4-2') {
+      if (q1Selection === 'yes') {
+      // show response message 1 for q4 if user answered yes to q1
+      document.getElementById('q4-yes-response-1').classList.remove('hidden');
+      d3.selectAll('#q4-yes-response-2, #q4-no-response-1, #q4-no-response-2').attr('class', 'hidden');
+      } else {
+      // show response message 2 for q4 if user answered no to q1
+      document.getElementById('q4-yes-response-2').classList.remove('hidden');
+      d3.selectAll('#q1-yes-response-2, #q4-no-response-1, #q4-no-response-2').attr('class', 'hidden');
+      }
+      d3.selectAll("#text-9, #path-9, .check7, #q4-1-desktop, #q4-1-mobile, #text-9M").transition().duration(700).style('opacity', 0);
+      d3.selectAll("#text-10, #path-10, .check8, #q4-2-desktop, #q4-2-mobile, #text-10M").transition().duration(700).style('opacity', 1);
+      }
   });
 });
